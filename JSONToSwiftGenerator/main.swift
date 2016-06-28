bdf4b771585255375c10807dc78149a26cb75e5c
Vlad Rusu <v@vladr.com>
2016-06-28 14:00:39 +0300

    Initial Commit

---
+++ JSONToSwiftGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JSONToSwiftGenerator
+//
+//  Created by Vlad Rusu on 6/28/16.
+//  Copyright © 2016 VladR. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
